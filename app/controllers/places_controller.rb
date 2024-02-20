1b5ef279bd136ba4345d5c987b5c05d8c5774c5b
Praveen Renganathan <praveenmanickam.renganathan@kellogg.northwestern.edu>
2024-02-20 19:13:04 +0000

    Update8

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,16 +1,16 @@
 class PlacesController < ApplicationController
 
-  def index
+    def index
   
-    @places = Place.all
+      @places = Place.all
   
     end
 
     def show
-      # find a Company
+     
       #@places = Place.find_by({"id" => 1})
       @places = Place.find_by({"id" => params["id"]})
-      # render companies/show view with details about Company
+      
     end
   
     def new
@@ -18,13 +18,10 @@ class PlacesController < ApplicationController
     end
   
     def create
-      # start with a new Company
+     
       @place = Place.new
-      # assign user-entered form data to Company's columns
       @place["name"] = params["name"]
-      # save Company row
       @place.save
-      # redirect user
       redirect_to "/places"
     end
 
