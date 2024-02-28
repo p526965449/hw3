cb09468acda5c6a1ca3a89bc4253b95fc8293b17
Praveen Renganathan <praveenmanickam.renganathan@kellogg.northwestern.edu>
2024-02-28 03:54:11 +0000

    Updat7

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,22 +2,18 @@ class EntriesController < ApplicationController
 
   def show
     @entry = Entry.find_by({"id" => params["id"]})
-    #@company = Company.find_by({"id" => @contact["company_id"]})
-    # render contacts/show view with details about Contact
+    @place = Place.find_by({"id" => @entry["company_id"]})
+
   end
-  # def create
-     
-  #   @place = Place.new
-  #   @place["name"] = params["name"]
-  #   @place.save
-  #   redirect_to "/places"
-  # end
+
+  def new
+  
+  end
+
   def create
     @entry = Entry.new
-
-    # assign user-entered form data to Contact's columns
     @entry["title"] = params["title"]
     @entry.save
-    redirect_to "/places"
+    redirect_to "/places/#{@entry["place_id"]}"
   end
 end
